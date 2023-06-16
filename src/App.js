9857480f9daab791daee606c9b6745b539134e65
francksefu <90141102+francksefu@users.noreply.github.com>
2023-06-16 23:16:30 +0200

    Add barre separate and fix styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {
-  Routes, Route, Link,
+  Routes, Route,
 } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getbook } from './redux/books/booksSlice';
 import BooksRender from './components/BooksRender';
 import Categories from './components/Categories';
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -14,18 +15,6 @@ function App() {
   }, [dispatch]);
   return (
     <>
-      <h1>Awesome books</h1>
-      <nav>
-        <ul className="ligne">
-          <li>
-            <Link to="/">books |</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories </Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
         <Route path="/" element={<BooksRender />} />
         <Route path="/categories" element={<Categories />} />
--- src/components/BooksRender.jsx
+++ src/components/BooksRender.jsx
@@ -1,11 +1,24 @@
 import BooksForm from './BooksForm';
 import BooksList from './BooksList';
+import imagete from '../imagete.png';
 
 const BooksRender = () => (
   <>
-    <BooksList />
-    <div className="Line" />
-    <BooksForm />
+    <div className="panel-bg">
+      <div className="bar-panel-bg">
+        <div>
+          <span><h2 className="titre">Bookstore CMS</h2></span>
+          <span className="books">BOOKS</span>
+          <span className="categorie">CATEGORIES</span>
+        </div>
+        <div>
+          <img src={imagete} alt="person" className="Oval-tete" />
+        </div>
+      </div>
+      <BooksList />
+      <div className="Line-bas" />
+      <BooksForm />
+    </div>
   </>
 );
 export default BooksRender;
